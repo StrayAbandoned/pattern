36d703c07c58319c3f6d1e32566395838b7a992c
StrayAbandoned <stray1988@mail.ru>
2022-12-23 14:35:27 +0300

    lesson6

---
+++ src/main/java/ru/lapshina/architecture/mapper/SimpleEntity.java
@@ -0,0 +1,10 @@
+package ru.lapshina.architecture.mapper;
+
+import lombok.Data;
+
+@Data
+public class SimpleEntity {
+    private Long id;
+    private String name;
+    private String property;
+}
